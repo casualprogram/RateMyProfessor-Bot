72506098dab48070e63b6f34ee02e03e198edfbd
casualprogram <113277994+casualprogram@users.noreply.github.com>
2023-07-19 00:58:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const puppeteer = require("puppeteer")
 const { log } = require('console')
 const readline = require("readline")
 const delay = require("./functions/delay")
- 
+const fetchSchoolId = require("./functions/fetchSchoolId")
+
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+
 
 async function start(){
-
+    fetchSchoolId()
 }
 
 start()
