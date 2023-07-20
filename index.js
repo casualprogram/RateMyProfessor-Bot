2b5f4b70228d657cdc7515fe87664d3217626375
casualprogram <113277994+casualprogram@users.noreply.github.com>
2023-07-20 14:12:19 -0400

    Update index.js
    Mnior tweak

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const { log } = require('console')
 const readline = require("readline")
 const delay = require("./functions/delay")
 const fetchSchoolId = require("./functions/fetchSchoolId")
+const analysisProfessor = require ("./functions/FetchProfessorReview")
+const fetchProfessorId = require ("./functions/fetchProfessorId")
 
 
 async function start(){
-    fetchSchoolId()
+    const schoolId = await fetchSchoolId()
+    const professorId = await fetchProfessorId(schoolId)
+    await analysisProfessor(professorId)
 }
 
 start()
